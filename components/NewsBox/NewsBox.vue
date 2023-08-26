971d968dd0c71f82c39c5555c9dc8c19d771ef37
leizhuzheng <leizhuzheng@qq.com>
2023-08-26 23:12:14 +0800

    找到公共模块定义components组件

---
+++ components/NewsBox/NewsBox.vue
@@ -0,0 +1,68 @@
+<template>
+	<view class="newsBox">
+		<view class="pic">
+			<image src="../../static/images/0.jpg" mode="aspectFill"></image>
+		</view>
+		<view class="text">
+			<view class="title">默认的新闻标题默认的新闻标题默认的新闻标题默认</view>
+			<view class="info">
+				<text>作者名称</text>
+				<text>998浏览</text>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "NewsBox",
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.newsBox {
+		display: flex;
+
+		.pic {
+			width: 230rpx;
+			height: 160rpx;
+
+			image {
+				width: 100%;
+				height: 100%;
+			}
+		}
+
+		.text {
+			flex: 1;
+			padding-left: 20rpx;
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+
+			.title {
+				font-size: 38rpx;
+				color: #333;
+				overflow: hidden;
+				text-overflow: ellipsis;
+				display: -webkit-box;
+				-webkit-line-clamp: 2;
+				-webkit-box-orient: vertical;
+			}
+
+			.info {
+				font-size: 26rpx;
+				color: #999;
+
+				text {
+					padding-right: 30rpx;
+				}
+			}
+		}
+	}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,10 +2,12 @@
 	<view class="home">
 		<scroll-view scroll-x class="navscroll" show-scrollbar="false">
 			<view class="item" v-for="(item,index) in 10" :key="index">国内</view>
-			
+
 		</scroll-view>
 		<view class="content">
-			<view class="row">每一行的新闻</view>
+			<view class="row"  v-for="(item,index) in 10" :key="index">
+				<NewsBox></NewsBox>
+			</view>
 		</view>
 	</view>
 </template>
@@ -27,24 +29,38 @@
 </script>
 
 <style lang="scss" scoped>
-.navscroll{
-	height: 100rpx;
-	background-color:#f7f8fa ;
-	white-space: nowrap;
-	::-webkit-scrollbar {
-			    display: none;
-			    width: 0 !important;
-			    height: 0 !important;
-			    -webkit-appearance: none;
-			    background: transparent;
-			    color: transparent;
-			  }
-	.item{
-		font-size: 40rpx;
-		display: inline-block;
-		line-height: 100rpx;
-		padding: 0 30rpx;
-		color: #333;
+	.navscroll {
+		height: 100rpx;
+		background-color: #f7f8fa;
+		white-space: nowrap;
+
+		/* 解决小程序和app滚动条的问题 */
+		/* #ifdef MP-WEIXIN || APP-PLUS */
+		::-webkit-scrollbar {
+			display: none;
+			width: 0 !important;
+			height: 0 !important;
+			-webkit-appearance: none;
+			background: transparent;
+			color: transparent;
+		}
+
+		/* #endif */
+
+		.item {
+			font-size: 40rpx;
+			display: inline-block;
+			line-height: 100rpx;
+			padding: 0 30rpx;
+			color: #333;
+		}
 	}
-}
+
+	.content {
+		padding: 30rpx;
+		.row {
+			border-bottom: 1px dotted #efefef;
+			padding: 20rpx 0;
+		}
+	}
 </style>
